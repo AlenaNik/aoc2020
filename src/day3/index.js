b239396fcf09a992710525c05d42e8251bc10cd6
AlenaNik <alena.nik.digital@gmail.com>
2020-12-04 13:28:21 +0100

    change logic for fifth case

--- src/day3/index.js
+++ src/day3/index.js
@@ -14,42 +14,25 @@ let multiply = (arr) => {
 // data
 const data = (arr) => arr.split("\n")
 // movement pattern
-const treeCounter1 = (someMap, right, down, tree) => {
-  let position = 0;
+const counter = (someMap, right, down, tree) => {
   return someMap.filter(function(row, index) {
-    if (index % down === 0) {
-      position = row[(index * right) % row.length]
-      return position === tree
-    }
+      if (index % down === 0) {
+        return row[index * right / down % row.length] === tree
+      }
   }).length
-}
-
-
-const treeCounter2 = (someMap, right, down, tree) => {
-  let count = 0;
-  let position = 0;
-  // 6818112000
-  someMap.forEach((row, index) => {
-    if (index === 0 || index % down !== 0) return count;
-    position = (position + right) % row.length;
-    if (row[position] === tree) {
-      count++
-    }
-  });
-  return count;
 }
 
 const goA = (input, step1, step2, char) => {
   // 228
-  return treeCounter1(data(input), step1, step2, char)
+  return counter(data(input), step1, step2, char)
 }
 
 const goB = (input, step1, step2, char) => {
-  const one = treeCounter2(data(input), 1, 1, char);
-  const two = treeCounter2(data(input), 3, 1, char);
-  const three = treeCounter2(data(input), 5, 1, char);
-  const four = treeCounter2(data(input), 7, 1, char);
-  const five = treeCounter2(data(input), 1, 2, char);
+  const one = counter(data(input), 1, 1, char);
+  const two = counter(data(input), 3, 1, char);
+  const three = counter(data(input), 5, 1, char);
+  const four = counter(data(input), 7, 1, char);
+  const five = counter(data(input), 1, 2, char);
   // 6818112000
   return multiply([one, two, three, four, five]);
 }
